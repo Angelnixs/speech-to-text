b87a8c7a90d2fda89ffbd1f317af70939275b52f
Angelnixs <juangon@hotmail.com>
2023-04-29 00:05:17 -0600

    volume

--- speechRecognition.js
+++ speechRecognition.js
@@ -1,12 +1,16 @@
 if ("webkitSpeechRecognition" in window) {
   // Initialize webkitSpeechRecognition
   let speechRecognition = new webkitSpeechRecognition();
+  var meter = null;
+  var recordingStarted = false;
+  var WIDTH = 500;
 
   // String for the Final Transcript
   let final_transcript = "";
 
   // Set the properties for the Speech Recognition object
   speechRecognition.continuous = true;
+  speechRecognition.maxAlternatives = 1;
   speechRecognition.interimResults = true;
   speechRecognition.lang = document.querySelector("#select_dialect").value;
 
@@ -46,13 +50,13 @@ if ("webkitSpeechRecognition" in window) {
   // Set the onClick property of the start button
   document.querySelector("#start").onclick = () => {
       // Start the Speech Recognition
+      recordingStarted = true;
       speechRecognition.start();
   };
   // Set the onClick property of the stop button
   document.querySelector("#stop").onclick = () => {
     speechRecognition.stop();
-
-    console.time('responsetime');
+    recordingStarted = false;
     // ====================
     // ====================
     // ====================
@@ -122,6 +126,119 @@ if ("webkitSpeechRecognition" in window) {
 
     xhr.send(data);
 }
+
+  navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
+    __log('No live audio input: ' + e);
+  });
+  
+  function startUserMedia(stream) {
+    const ctx = new AudioContext();
+    const analyser = ctx.createAnalyser();
+    const streamNode = ctx.createMediaStreamSource(stream);
+    streamNode.connect(analyser);
+  
+    // Create a new volume meter and connect it.
+    meter = createAudioMeter(ctx);
+    streamNode.connect(meter);
+    console.log(meter)
+    drawLoop();
+  
+  }
+  
+  // Create pitch bar
+  function drawLoop( time ) {
+    
+    var pitchVolume = meter.volume*WIDTH*1.4;
+    // console.log(meter.volume)
+    var width = 0;
+  
+    // Pitch detection minimum volume
+    var minimum_volume = 130;
+  
+    // Get width if Recording started
+    if(recordingStarted){
+  
+      if(pitchVolume < minimum_volume){
+         width = 0;
+      }else if(pitchVolume >= minimum_volume && pitchVolume < (minimum_volume+20) ){
+         width = 10;
+      }else if(pitchVolume >= (minimum_volume+20) && pitchVolume < (minimum_volume+40)){
+         width = 20;
+      }else if(pitchVolume >= (minimum_volume+40) && pitchVolume < (minimum_volume+60)){
+         width = 30;
+      }else if(pitchVolume >= (minimum_volume+60) && pitchVolume < (minimum_volume+80)){
+         width = 40;
+      }else if(pitchVolume >= (minimum_volume+80) && pitchVolume < (minimum_volume+100)){
+         width = 50;
+      }else if(pitchVolume >= (minimum_volume+100) && pitchVolume < (minimum_volume+120)){
+         width = 60;
+      }else if(pitchVolume >= (minimum_volume+120) && pitchVolume < (minimum_volume+140)){
+         width = 70;
+      }else if(pitchVolume >= (minimum_volume+140) && pitchVolume < (minimum_volume+160)){
+         width = 80;
+      }else if(pitchVolume >= (minimum_volume+160) && pitchVolume < (minimum_volume+180)){
+         width = 90;
+      }else if(pitchVolume >= (minimum_volume+180)){
+         width = 100;
+      }
+  
+    }
+    $("#volumenumber").html(`<span>${pitchVolume.toFixed(1)}</span>`);
+    // Update width
+    document.getElementById('voiceVolume').style.width = width+'%';
+    setTimeout(function(){
+      rafID = window.requestAnimationFrame( drawLoop );
+    }, 1000 );
+  }
 } else {
   console.log("Speech Recognition Not Available");
 }
+
+// var meter = null;
+// var WIDTH = 500;
+// var recordingStarted = false;
+
+// // initialize SpeechRecognition object
+// let recognition = new webkitSpeechRecognition();
+// recognition.maxAlternatives = 1;
+// recognition.continuous = true;
+
+// // Detect the said words
+// recognition.onresult = e => {
+
+//   var current = event.resultIndex;
+
+//   // Get a transcript of what was said.
+//   var transcript = event.results[current][0].transcript;
+
+//   // Add the current transcript with existing said values
+//   var noteContent = $('#saidwords').val();
+//   noteContent += ' ' + transcript;
+//   $('#saidwords').val(noteContent);
+
+// }
+
+// // Stop recording
+// function stopSpeech(){
+
+//   // Change status
+//   $('#status').text('Recording Stopped.');
+//   recordingStarted = false;
+
+//   // Stop recognition
+//   recognition.stop();
+// }
+
+// // Start recording
+// function startSpeech(){
+//   try{ // calling it twice will throw..
+//     $('#status').text('Recording Started.'); 
+//     $('#saidwords').val('');
+//     recordingStarted = true;
+
+//     // Start recognition
+//     recognition.start();
+//   }
+//   catch(e){}
+// }
+
